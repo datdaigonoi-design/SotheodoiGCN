ce497f45cac1aaa0d9b7f0ffbd48cf5d8f629354
datdaigonoi-design <datdaigonoi@gmail.com>
2026-03-05 22:35:45 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,8 @@
 // js/supabase.js
 
-const SUPABASE_URL = "https://xxxxx.supabase.co"
-const SUPABASE_KEY = "anon-public-key"
+const SUPABASE_URL = "https://nujriavvxlnxieeminqw.supabase.co"
+const SUPABASE_KEY = "const SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51anJpYXZ2eGxueGllZW1pbnF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3MTc5MDQsImV4cCI6MjA4ODI5MzkwNH0.MZk3da7WpOTZ8zeEdLZvzrgV5wjE1diWL6kNOA7sCDM"
+"
 
 window.supabaseClient = supabase.createClient(
 SUPABASE_URL,
